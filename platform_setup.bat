e35503bcf0b808b0341d545d993ba313d5d4557a
Raghu Ram <raghu.duddumpudi@gmail.com>
2022-04-10 08:25:57 +0530

    Added batch files for windows

---
+++ platform_setup.bat
@@ -0,0 +1,20 @@
+# Set the Enviroment variables 
+
+
+set DEPLOY_IMAGE=rramur/plat-c:latest
+
+set SHARED_VOLUME_PATH=C:\Code
+
+
+
+#Clean container and images
+
+docker-compose down
+
+docker rm -f platform_c
+
+
+
+# Bring up containers  
+
+docker-compose up -d
